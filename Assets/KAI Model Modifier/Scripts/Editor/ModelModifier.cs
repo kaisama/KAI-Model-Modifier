dbf6b55fd7fe439dbe5331041cfadde0471e3863
kaisama <0ultrakai0@gmail.com>
2020-01-19 15:09:30 +0200

    initial commit

---
+++ Assets/KAI Model Modifier/Scripts/Editor/ModelModifier.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+
+namespace KAI
+{
+    public class ModelModifier : EditorWindow
+    {
+        private static ModelModifier Instance;
+
+        private string[] PivotTypes = { "No Change", "Center", "Center Top", "Center Bottom", "Center Right", "Center Left", "Center Front", "Center Back" };
+        private int CurrentPivotType;
+
+        private GameObject Model;
+        private GameObject NewRoot;
+        private string NewRootName = "ROOT";
+        private PIVOT_TYPE NewPivot;
+
+        private bool UseNewScale;
+        private Vector3 NewScale;
+
+        private bool UseNewRotation;
+        private Vector3 NewRotation;
+
+        private ListRequest listRequest;
+        private AddRequest addRequest;
+        private bool CheckComplete = false;
+        private bool AttemptingToAdd = false;
+        private bool ExporterFound = false;
+
+        [MenuItem("Custom Tools/Model Modifier")]
+        public static void OpenWindow()
+        {
+            if (Instance == null)
+            {
+                Instance = GetWindow<ModelModifier>();
+                Instance.minSize = new Vector2(640, 480);
+            }
+            else
+            {
+                EditorWindow.FocusWindowIfItsOpen<ModelModifier>();
+            }
+
+            Instance.titleContent = new GUIContent("Model Modifier");
+        }
+
+
+        private void OnEnable()
+        {
+            listRequest = Client.List();
+            EditorApplication.update += CheckFBXExporter;
+        }
+
+        private void CheckFBXExporter()
+        {
+            if (listRequest.IsCompleted)
+            {
+                if (listRequest.Status == StatusCode.Success)
+                    foreach (var package in listRequest.Result)
+                    {
+                        if (package.name.Equals("com.unity.formats.fbx"))
+                        {
+                            ExporterFound = true;
+                            return;
+                        }
+                    }
+                else if (listRequest.Status >= StatusCode.Failure)
+                    Debug.Log(listRequest.Error.message);
+
+                EditorApplication.update -= CheckFBXExporter;
+                CheckComplete = true;
+            }
+        }
+
+        private void AddFBXExporter()
+        {
+            if (addRequest.IsCompleted)
+            {
+                if (addRequest.Status == StatusCode.Success)
+                    ExporterFound = true;
+                else if (addRequest.Status >= StatusCode.Failure)
+                    Debug.Log(listRequest.Error.message);
+
+                EditorApplication.update -= AddFBXExporter;
+                CheckComplete = true;
+            }
+        }
+
+        private void OnInspectorUpdate()
+        {
+            if (ExporterFound == false && AttemptingToAdd == false)
+            {
+                addRequest = Client.Add("com.unity.formats.fbx");
+                AttemptingToAdd = true;
+                EditorApplication.update += AddFBXExporter;
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (ExporterFound)
+            {
+                DrawModelSection();
+                DrawCustomizationSection();
+
+                if (Model)
+                {
+                    GUILayout.BeginArea(new Rect(5, position.height - 60, position.width - 12, 60));
+
+                    if (GUILayout.Button("Apply Settings"))
+                    {
+                        CreateModel();
+                    }
+
+                    GUILayout.Space(5);
+
+                    if (GUILayout.Button("Export Model"))
+                    {
+                        if (NewRoot == null)
+                        {
+                            CreateModel();
+                        }
+
+                        if (NewRoot)
+                        {
+                            Selection.activeGameObject = NewRoot;
+
+                            EditorApplication.ExecuteMenuItem("GameObject/Export To FBX...");
+                        }
+                    }
+
+                    GUILayout.EndArea();
+                }
+            }
+            else
+            {
+                GUIStyle s = new GUIStyle();
+                s.fontSize = 20;
+                s.wordWrap = true;
+                EditorGUILayout.LabelField("ATTEMPTING TO IMPORT FBX Exporter please check your internet connection", s, GUILayout.Width(position.width - 10), GUILayout.Height(100));
+            }
+        }
+
+        private void DrawModelSection()
+        {
+            GUILayout.BeginArea(new Rect(2, 3, position.width - 6, 27), EditorStyles.helpBox);
+            GUILayout.Space(1);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Model To Modify", EditorStyles.helpBox, GUILayout.Width(((position.width - 6) / 4)));
+            Model = (GameObject)EditorGUILayout.ObjectField(Model, typeof(GameObject), true, GUILayout.Width(((position.width - 6) * 3 / 4) - 15));
+            EditorGUILayout.EndHorizontal();
+            GUILayout.EndArea();
+        }
+
+        private void DrawCustomizationSection()
+        {
+            GUILayout.BeginArea(new Rect(2, 35, position.width - 6, position.height - 40), EditorStyles.helpBox);
+            GUILayout.Space(1);
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("New Root Name", EditorStyles.helpBox, GUILayout.Width(((position.width - 6) / 4)));
+            NewRootName = EditorGUILayout.TextField(NewRootName, EditorStyles.textField, GUILayout.Width(((position.width - 6) * 3 / 4) - 15), GUILayout.Height(20));
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("New Pivot Location", EditorStyles.helpBox, GUILayout.Width(((position.width - 6) / 4)));
+            CurrentPivotType = EditorGUILayout.Popup(CurrentPivotType, PivotTypes, GUILayout.Width(((position.width - 6) * 3 / 4) - 15), GUILayout.Height(20));
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Use New Scale", EditorStyles.helpBox, GUILayout.Width(((position.width - 6) / 4)));
+            UseNewScale = EditorGUILayout.Toggle(UseNewScale, GUILayout.Width(((position.width - 6) * 4 / 4) - 15));
+            EditorGUILayout.EndHorizontal();
+
+            if (UseNewScale)
+            {
+                EditorGUILayout.Space();
+
+                NewScale = EditorGUILayout.Vector3Field("New Scale", NewScale, GUILayout.Width(((position.width - 6) * 4 / 4) - 15), GUILayout.Height(20));
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Use New Rotation", EditorStyles.helpBox, GUILayout.Width(((position.width - 6) / 4)));
+            UseNewRotation = EditorGUILayout.Toggle(UseNewRotation, GUILayout.Width(((position.width - 6) * 4 / 4) - 15));
+            EditorGUILayout.EndHorizontal();
+
+            if (UseNewRotation)
+            {
+                EditorGUILayout.Space();
+
+                NewRotation = EditorGUILayout.Vector3Field("New Rotation", NewRotation, GUILayout.Width(((position.width - 6) * 4 / 4) - 15), GUILayout.Height(20));
+            }
+
+            GUILayout.EndArea();
+        }
+
+        private void CreateModel()
+        {
+            switch (CurrentPivotType)
+            {
+                case 0:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.NO_CHANGE);
+                    }
+                    break;
+                case 1:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER);
+                    }
+                    break;
+                case 2:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER_TOP);
+                    }
+                    break;
+                case 3:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER_BOTTOM);
+                    }
+                    break;
+                case 4:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER_RIGHT);
+                    }
+                    break;
+                case 5:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER_LEFT);
+                    }
+                    break;
+                case 6:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER_FRONT);
+                    }
+                    break;
+                case 7:
+                    {
+                        NewRoot = ModelUtils.CreateNewRoot(Model, PIVOT_TYPE.CENTER_BACK);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if (NewRootName.Length == 0)
+            {
+                NewRootName = Model.name;
+            }
+            else
+            {
+                NewRoot.name = NewRootName;
+            }
+
+            if (UseNewScale)
+            {
+                NewRoot.transform.localScale = NewScale;
+            }
+
+            if (UseNewRotation)
+            {
+                NewRoot.transform.GetChild(0).localEulerAngles = NewRotation;
+            }
+        }
+    }
+}
---
+++ Assets/KAI Model Modifier/Scripts/ModelUtils.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KAI
+{
+    public enum PIVOT_TYPE
+    {
+        NO_CHANGE,
+        CENTER,
+        CENTER_TOP,
+        CENTER_BOTTOM,
+        CENTER_RIGHT,
+        CENTER_LEFT,
+        CENTER_FRONT,
+        CENTER_BACK
+    }
+
+    public class ModelUtils : MonoBehaviour
+    {
+        public static Bounds CalculateBounds(GameObject obj)
+        {
+            Bounds res;
+
+            obj.transform.position = Vector3.zero;
+
+            var renderers = obj.GetComponentsInChildren<Renderer>();
+            res = renderers[0].bounds;
+
+            for (int i = 1; i < renderers.Length; i++)
+            {
+                Bounds b = renderers[i].bounds;
+                Vector3 max = Vector3.Max(res.max, b.max);
+                Vector3 min = Vector3.Min(res.min, b.min);
+
+                res = new Bounds((max + min) * 0.5f, max - min);
+            }
+
+            return res;
+        }
+
+        public static Vector3 GetCenter(GameObject obj)
+        {
+            Vector3 res = CalculateBounds(obj).center;
+
+            return res;
+        }
+
+        public static Vector3 GetBottomCenter(GameObject obj)
+        {
+            var bounds = CalculateBounds(obj);
+
+            Vector3 res = bounds.center;
+            res.y -= bounds.size.y / 2;
+
+            return res;
+        }
+
+        public static Vector3 GetTopCenter(GameObject obj)
+        {
+            var bounds = CalculateBounds(obj);
+
+            Vector3 res = bounds.center;
+            res.y += bounds.size.y / 2;
+
+            return res;
+        }
+
+        public static Vector3 GetRightCenter(GameObject obj)
+        {
+            var bounds = CalculateBounds(obj);
+
+            Vector3 res = bounds.center;
+            res.x += bounds.size.x / 2;
+
+            return res;
+        }
+
+        public static Vector3 GetLeftCenter(GameObject obj)
+        {
+            var bounds = CalculateBounds(obj);
+
+            Vector3 res = bounds.center;
+            res.x -= bounds.size.x / 2;
+
+            return res;
+        }
+
+        public static Vector3 GetFrontCenter(GameObject obj)
+        {
+            var bounds = CalculateBounds(obj);
+
+            Vector3 res = bounds.center;
+            res.z -= bounds.size.z / 2;
+
+            return res;
+        }
+
+        public static Vector3 GetBackCenter(GameObject obj)
+        {
+            var bounds = CalculateBounds(obj);
+
+            Vector3 res = bounds.center;
+            res.z += bounds.size.z / 2;
+
+            return res;
+        }
+
+        public static GameObject CreateNewRoot(GameObject obj, PIVOT_TYPE pivot)
+        {
+            GameObject o = Instantiate<GameObject>(obj, Vector3.zero, Quaternion.identity);
+            o.name = obj.name;
+            GameObject res = null;
+            switch (pivot)
+            {
+                case PIVOT_TYPE.NO_CHANGE:
+                    {
+                        o.transform.position = Vector3.zero;
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+                        o.transform.SetParent(o2.transform);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER:
+                    {
+                        Vector3 pivotPos = GetCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER_TOP:
+                    {
+                        Vector3 pivotPos = GetTopCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER_BOTTOM:
+                    {
+                        Vector3 pivotPos = GetBottomCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER_LEFT:
+                    {
+                        Vector3 pivotPos = GetLeftCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER_RIGHT:
+                    {
+                        Vector3 pivotPos = GetRightCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER_FRONT:
+                    {
+                        Vector3 pivotPos = GetFrontCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                case PIVOT_TYPE.CENTER_BACK:
+                    {
+                        Vector3 pivotPos = GetBackCenter(o);
+
+                        var o1 = new GameObject("ROOT");
+                        var o2 = new GameObject(o.name + " ROOT");
+                        o2.transform.SetParent(o1.transform);
+
+                        o2.transform.position = pivotPos;
+
+                        o.transform.SetParent(o2.transform);
+
+                        o2.transform.Translate(o1.transform.position - o2.transform.position, Space.World);
+
+                        res = o1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
